4f8f36cc390854738a3a60729741d7fedd07782f
Alex Alexandrovich <javafirst17@gmail.com>
2024-11-20 20:39:37 +0300

    Update and rename notepad.c to main.c

---
+++ main.c
@@ -0,0 +1,138 @@
+#include <gtk/gtk.h>
+
+
+GtkWidget *text_view; // Текстовый виджет
+
+// Функция для открытия файла
+void on_menu_open_activate(GtkMenuItem *menuitem, gpointer user_data) {
+    GtkWidget *dialog;
+    GtkTextBuffer *buffer;
+    char *filename;
+    gchar *content;
+    gsize length;
+
+    GtkWidget *window = GTK_WIDGET(user_data);
+    dialog = gtk_file_chooser_dialog_new("Открыть файл",
+                                         GTK_WINDOW(window),
+                                         GTK_FILE_CHOOSER_ACTION_OPEN,
+                                         "_Отмена", GTK_RESPONSE_CANCEL,
+                                         "_Открыть", GTK_RESPONSE_ACCEPT,
+                                         NULL);
+
+    if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
+        GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+        filename = gtk_file_chooser_get_filename(chooser);
+
+        if (g_file_get_contents(filename, &content, &length, NULL)) {
+            buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+            gtk_text_buffer_set_text(buffer, content, -1);
+            g_free(content);
+        }
+
+        g_free(filename);
+    }
+
+    gtk_widget_destroy(dialog);
+}
+
+// Функция для сохранения файла
+void on_menu_save_activate(GtkMenuItem *menuitem, gpointer user_data) {
+    GtkWidget *dialog;
+    GtkTextBuffer *buffer;
+    GtkTextIter start, end;
+    gchar *content;
+    char *filename;
+
+    GtkWidget *window = GTK_WIDGET(user_data);
+    dialog = gtk_file_chooser_dialog_new("Сохранить файл",
+                                         GTK_WINDOW(window),
+                                         GTK_FILE_CHOOSER_ACTION_SAVE,
+                                         "_Отмена", GTK_RESPONSE_CANCEL,
+                                         "_Сохранить", GTK_RESPONSE_ACCEPT,
+                                         NULL);
+
+    if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
+        GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+        filename = gtk_file_chooser_get_filename(chooser);
+
+        buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+        gtk_text_buffer_get_start_iter(buffer, &start);
+        gtk_text_buffer_get_end_iter(buffer, &end);
+        content = gtk_text_buffer_get_text(buffer, &start, &end, FALSE);
+
+        if (g_file_set_contents(filename, content, -1, NULL)) {
+            g_print("Файл сохранен: %s\n", filename);
+        } else {
+            g_print("Ошибка при сохранении файла: %s\n", filename);
+        }
+
+        g_free(content);
+        g_free(filename);
+    }
+
+    gtk_widget_destroy(dialog);
+}
+
+// Функция выхода
+void on_menu_exit_activate(GtkMenuItem *menuitem, gpointer user_data) {
+    gtk_main_quit();
+}
+
+int main(int argc, char *argv[])
+{
+    // GtkBuilder      *builder; 
+    // GtkWidget       *window;
+
+    // gtk_init(&argc, &argv);
+
+    // builder = gtk_builder_new();
+    // gtk_builder_add_from_file (builder, "notepad.glade", NULL);
+
+    // window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+
+    // gtk_builder_connect_signals(builder, NULL);
+    
+    // // Quit the app when the window is closed.
+    // g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+
+    // g_object_unref(builder);
+
+    // gtk_widget_show(window);                
+    // gtk_main();
+
+    // return 0;
+
+        GtkBuilder *builder;
+    GtkWidget *window;
+    GError *error = NULL;
+
+    gtk_init(&argc, &argv);
+
+    // Загрузка интерфейса из файла Glade
+    builder = gtk_builder_new();
+    if (!gtk_builder_add_from_file(builder, "editor.glade", &error)) {
+        g_printerr("Ошибка при загрузке файла интерфейса: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
+    }
+
+    // Получение виджетов
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+    text_view = GTK_WIDGET(gtk_builder_get_object(builder, "text_view"));
+
+    // Привязка сигналов
+    gtk_builder_connect_signals(builder, window);
+
+    g_signal_connect(gtk_builder_get_object(builder, "menu_open"), "activate", G_CALLBACK(on_menu_open_activate), window);
+    g_signal_connect(gtk_builder_get_object(builder, "menu_save"), "activate", G_CALLBACK(on_menu_save_activate), window);
+    g_signal_connect(gtk_builder_get_object(builder, "menu_exit"), "activate", G_CALLBACK(on_menu_exit_activate), NULL);
+
+    // Отображение главного окна
+    gtk_widget_show_all(window);
+
+    gtk_main();
+
+    return 0;
+}
+
